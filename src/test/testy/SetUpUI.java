93b21a9eeba2c4643b3bacc38cfae5d6c5818eca
astor4ever <mateusz.z.grabowski@gmail.com>
2019-01-25 09:53:28 +0100

    new test check each pair

--- src/test/testy/SetUpUI.java
+++ src/test/testy/SetUpUI.java
@@ -21,21 +21,20 @@ public class SetUpUI {
     @BeforeTest
     public void setUp() throws MalformedURLException {
 
-        String localRun = PropertyManager.getInstance().getLocalRun();
+       // String localRun = PropertyManager.getInstance().getLocalRun();
         DesiredCapabilities dc = DesiredCapabilities.chrome();
-        if(localRun.equals("true")){
-            PropertyManager.getInstance().setChromeWebDriver();
-            driver = new ChromeDriver();
-        }else {
+       // if(localRun.equals("true")){
+      //      PropertyManager.getInstance().setChromeWebDriver();
+      //      driver = new ChromeDriver();
             if (System.getProperty("browser").equals("firefox")) {
                 dc = DesiredCapabilities.firefox();
             }
             String host = System.getProperty("seleniumHubHost");
             driver = new RemoteWebDriver(new URL("http://" + host + ":4444/wd/hub"), dc);
         }
-        driver.get("http://dv.investimetric.io:81/");
-        System.out.println("Browser launched and navigated to Investimetric");
-    }
+//        driver.get("http://dv.investimetric.io:81/");
+//        System.out.println("Browser launched and navigated to Investimetric");
+//    }
 
 
     @AfterTest
