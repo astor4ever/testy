cdf8f53935d07aea4f4c57a23a9c00973e810456
astor4ever <mateusz.z.grabowski@gmail.com>
2019-01-31 11:16:44 +0100

    added new driver manager and change setup

--- src/test/testy/UITests.java
+++ src/test/testy/UITests.java
@@ -26,7 +26,7 @@ public class UITests extends SetUpBrowser{
     public void checkTitle() throws InterruptedException {
         mp = PageFactory.initElements(driver,MainPage.class);
         Thread.sleep(1500);
-        assertEquals(mp.getOrderbookText(),"Orderbookaaa");
+        assertEquals(mp.getOrderbookText(),"Orderbook");
         System.out.println("Orderbook is displayed.");
     }
 
