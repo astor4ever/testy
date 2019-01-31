7bcfc4ca573901b8edeec0d90f600d091e794f7a
astor4ever <mateusz.z.grabowski@gmail.com>
2019-01-31 12:17:56 +0100

    added new driver manager and change setup

--- src/test/testy/utilities/ScreenshotListener.java
+++ src/test/testy/utilities/ScreenshotListener.java
@@ -26,7 +26,7 @@ public class ScreenshotListener extends TestListenerAdapter {
                 try {
                     String reportDirectory;
                     if (System.getProperty("localRun") != null) {
-                        reportDirectory = "/home/dev/workspace/testy/front/target/surefire-reports";
+                        reportDirectory = "\\home\\dev\\workspace\\testy\\front\\target\\surefire-reports";
                     } else {
                         reportDirectory = new File(System.getProperty("user.dir")).getAbsolutePath() + "/target/surefire-reports";
                     }
@@ -37,11 +37,8 @@ public class ScreenshotListener extends TestListenerAdapter {
                     Reporter.log("<a href='" + destFile.getAbsolutePath() + "'> <img src='" + destFile.getAbsolutePath() + "' height='100' width='100'/> </a>");
                 } catch (IOException e) {
                     e.printStackTrace();
-                    System.out.println(e.getMessage());
                 }
-                System.out.println("SCREEN SAVED");
             }
-        System.out.println("SCREEN SAVED2");
     }
 
 
