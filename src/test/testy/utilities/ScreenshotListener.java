2f7553155c8edd80dd16ae05d168aeaded379d81
astor4ever <mateusz.z.grabowski@gmail.com>
2019-01-31 13:37:18 +0100

    added new driver manager and change setup

--- src/test/testy/utilities/ScreenshotListener.java
+++ src/test/testy/utilities/ScreenshotListener.java
@@ -25,9 +25,9 @@ public class ScreenshotListener extends TestListenerAdapter {
                 File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
                 try {
                     String reportDirectory = new File(System.getProperty("user.dir")).getAbsolutePath();
-                    File destFile = new File(reportDirectory + "/test-output/failed_screens/" + methodName + "_" + formater.format(calendar.getTime()) + ".png");
+                    File destFile = new File(reportDirectory + "/test-output/" + methodName + "_" + formater.format(calendar.getTime()) + ".png");
                     FileUtils.copyFile(scrFile, destFile);
-                    Reporter.log("<a href='" + destFile.getAbsolutePath().substring(33) + "'> <img src='" + destFile.getAbsolutePath().substring(33) + "' height='100' width='100'/> </a>");
+                    Reporter.log("<a href='" + reportDirectory+destFile.getAbsolutePath().substring(47) + "'> <img src='" + reportDirectory+destFile.getAbsolutePath().substring(47) + "' height='100' width='100'/> </a>");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
