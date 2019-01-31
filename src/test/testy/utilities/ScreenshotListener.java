98b3b82c3a8c65b035e2e74d2361a3e94fc9e085
astor4ever <mateusz.z.grabowski@gmail.com>
2019-01-31 11:57:02 +0100

    added new driver manager and change setup

--- src/test/testy/utilities/ScreenshotListener.java
+++ src/test/testy/utilities/ScreenshotListener.java
@@ -16,28 +16,32 @@ public class ScreenshotListener extends TestListenerAdapter {
 
     @Override
     public void onTestFailure (ITestResult result) {
-        ITestContext context = result.getTestContext();
-        this.driver = (WebDriver)context.getAttribute("driver");
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
-        String methodName = result.getName();
-        if(!result.isSuccess()){
-            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-            try {
-                String reportDirectory;
-                if(System.getProperty("localRun") != null) {
-                    reportDirectory = "/home/dev/workspace/testy/front/target/surefire-reports";
-                }else{
-                    reportDirectory = new File(System.getProperty("user.dir")).getAbsolutePath() + "/target/surefire-reports";
+        try {
+            ITestContext context = result.getTestContext();
+            this.driver = (WebDriver) context.getAttribute("driver");
+            Calendar calendar = Calendar.getInstance();
+            SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
+            String methodName = result.getName();
+            if (!result.isSuccess()) {
+                File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+                try {
+                    String reportDirectory;
+                    if (System.getProperty("localRun") != null) {
+                        reportDirectory = "/home/dev/workspace/testy/front/target/surefire-reports";
+                    } else {
+                        reportDirectory = new File(System.getProperty("user.dir")).getAbsolutePath() + "/target/surefire-reports";
+                    }
+                    System.out.println("REPORT DIRECTORY IS:" + reportDirectory);
+                    File destFile = new File(reportDirectory + "/failure_screenshots/" + methodName + "_" + formater.format(calendar.getTime()) + ".png");
+                    System.out.println("DEST FILE" + destFile);
+                    FileUtils.copyFile(scrFile, destFile);
+                    Reporter.log("<a href='" + destFile.getAbsolutePath() + "'> <img src='" + destFile.getAbsolutePath() + "' height='100' width='100'/> </a>");
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                System.out.println("REPORT DIRECTORY IS:"+reportDirectory);
-                File destFile = new File(reportDirectory +"/failure_screenshots/"+methodName+"_"+formater.format(calendar.getTime())+".png");
-                System.out.println("DEST FILE"+destFile);
-                FileUtils.copyFile(scrFile, destFile);
-                Reporter.log("<a href='"+ destFile.getAbsolutePath() + "'> <img src='"+ destFile.getAbsolutePath() + "' height='100' width='100'/> </a>");
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
     }
 
